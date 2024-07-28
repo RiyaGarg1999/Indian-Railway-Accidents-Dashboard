77f18bd8437d8f25c6744ce8b5df3a575eb670c5
Riya Garg <54986278+riya-coder@users.noreply.github.com>
2024-07-28 19:16:32 +0530

    Update Moon.jsx

--- Pseudocode/frontend/Moon.jsx
+++ Pseudocode/frontend/Moon.jsx
@@ -1,1 +1,20 @@
+// Import Necessary Modules
+import React from 'react';
+
+// Define Moon Component
+const Moon = () => {
+    // Return JSX
+    return (
+        <svg 
+            className='mode-icon moon-icon' // Add class for styling
+            xmlns="http://www.w3.org/2000/svg" // Set XML namespace
+            viewBox="0 0 30 30" // Set viewBox for scaling
+        >    
+            <path d="..."/> // SVG path data for moon icon
+        </svg>
+    );     
+}
+
+// Export Moon Component
+export default Moon;
 
